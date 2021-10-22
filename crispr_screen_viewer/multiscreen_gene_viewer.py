6d4b095114c195986b7b570f65ef286b440d1aa0
johnc.thomas <jct61@cam.ac.uk>
2021-10-22 15:16:17 +0100

    Refactor Dataset.metadata->.comparisons. Added Timepoint filter

--- crispr_screen_viewer/multiscreen_gene_viewer.py
+++ crispr_screen_viewer/multiscreen_gene_viewer.py
@@ -49,7 +49,7 @@ def launch(source_directory:Union[str, os.PathLike], port, debug):
         style={'height':'800px'}
     )
 
-    table = Div([create_datatable(columns_if_no_df=data_set.metadata.columns)],
+    table = Div([create_datatable(columns_if_no_df=data_set.comparisons.columns)],
                 id='table-div', className="u-full-width", style={'margin-bottom':'30px'})
 
     # select genes by name, and comparisons FDR in selected samples
@@ -85,7 +85,7 @@ def launch(source_directory:Union[str, os.PathLike], port, debug):
         Div([
             Div(get_reg_stat_selectors(app),
                 style={'display':'inline-block', 'width':'170px','vertical-align':'top'}),
-            Div(get_data_source_selector(data_set.data_sources), style={'display':'inline-block'})
+            Div(get_data_source_selector(data_set), style={'display':'inline-block'})
         ])
     ])
 
@@ -118,7 +118,8 @@ def launch(source_directory:Union[str, os.PathLike], port, debug):
 
     # make the dropdowns for filtering
     filter_dropdowns = []
-    filter_cols = ['Treatment', 'Experiment ID', 'KO', 'Cell line', 'Library', 'Source']
+    filter_cols = ['Treatment', 'Experiment ID', 'KO', 'Cell line',
+                   'Library', 'Source', 'Timepoint']
     for col in filter_cols:
         filter_dropdowns.append(
             html.Div([dcc.Dropdown(
@@ -126,8 +127,8 @@ def launch(source_directory:Union[str, os.PathLike], port, debug):
                 placeholder='Filter by '+col,
                 multi=True,
                 style={'height':'80px', 'width':'220px'},
-                value=[],
-                options=[{'label':v, 'value':v} for v in sorted(data_set.metadata[col].unique())]
+                value=[] if col != 'Timepoint' else ['endpoints'],
+                options=[{'label':v, 'value':v} for v in sorted(data_set.comparisons[col].unique())]
             )], style={'display':'inline-block'})
         )
 
@@ -169,7 +170,7 @@ def launch(source_directory:Union[str, os.PathLike], port, debug):
         data_tabs = data_set.get_score_fdr(score_type, fdr_type, selected_data_sources)
 
         # Identify data sources that are unavailable for selected analyses
-        available_sources = data_set.metadata.loc[data_tabs['fdr'].columns, 'Source'].unique()
+        available_sources = data_set.comparisons.loc[data_tabs['fdr'].columns, 'Source'].unique()
         missing_data_sources = [d for d in selected_data_sources if d not in available_sources]
         if missing_data_sources:
             missing_data_sources = "(unavailable with current analysis type: "+', '.join(missing_data_sources)+')'
@@ -178,10 +179,10 @@ def launch(source_directory:Union[str, os.PathLike], port, debug):
 
         # get boolean masks for which comparisons to include in the charts
         # first get comparisons filtered by metadata filters
-        comparison_mask = pd.Series(True, index=data_set.metadata.index)
+        comparison_mask = pd.Series(True, index=data_set.comparisons.index)
         for filter_id, values in zip(filter_cols, filters):
             if values:
-                comparison_mask = comparison_mask & data_set.metadata[filter_id].isin(values)
+                comparison_mask = comparison_mask & data_set.comparisons[filter_id].isin(values)
 
         # then by FDR threshold
         fdr_mask = (data_tabs['fdr'].loc[selected_genes] < fdr_thresh).any()
@@ -194,7 +195,7 @@ def launch(source_directory:Union[str, os.PathLike], port, debug):
             trace_order = filtered_scores.mean().sort_values().index.values
         elif order_by in order_by_categories[1:]:
             # subset the metadata to included comps and then sort by the order_by
-            trace_order = data_set.metadata.loc[filtered_scores.columns, order_by].sort_values().index
+            trace_order = data_set.comparisons.loc[filtered_scores.columns, order_by].sort_values().index
         else: # this shouldn't happen, though maybe I should have a "whatever" order option
             trace_order = filtered_scores.columns.values
 
@@ -211,12 +212,12 @@ def launch(source_directory:Union[str, os.PathLike], port, debug):
             )
         # add the boxplot traces if required
         if show_boxplots:
-            colour_map = get_colour_map(data_set.metadata.loc[:, color_by].unique())
+            colour_map = get_colour_map(data_set.comparisons.loc[:, color_by].unique())
             for col in trace_order:
                 ys = data_tabs['score'][col]
                 xs = ys[:]
                 xs[:] = col
-                color_group = data_set.metadata.loc[col, color_by]
+                color_group = data_set.comparisons.loc[col, color_by]
                 fig.add_trace(
                     go.Box(x=xs, y=ys, name=color_group, boxpoints=False,
                            line=dict(color=colour_map[color_group]))
@@ -235,7 +236,7 @@ def launch(source_directory:Union[str, os.PathLike], port, debug):
         selected_stats.insert(0, 'Comparison', selected_stats.index)
         cols_oi = ['Experiment ID', 'Treatment', 'Dose', 'KO', 'Growth inhibition %', 'Days grown',
                    'Cell line', 'Library']
-        selected_metadata = data_set.metadata.loc[selected_stats.index, cols_oi]
+        selected_metadata = data_set.comparisons.loc[selected_stats.index, cols_oi]
         data_table_data = pd.concat([selected_stats, selected_metadata], axis=1)
         dtable = create_datatable(data_table_data)
 
