25f4dce0c32371a940079655dea4102982ead883
johnc.thomas <jct61@cam.ac.uk>
2022-03-17 17:32:01 +0000

    requires, scripts, version bump.

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'crispr_screen_viewer',
-    version = '0.5.0',
+    version = '0.6.0',
     author = 'John C. Thomas',
     author_email = 'jcthomas000@gmail.com',
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'junk_probably']),
     #todo review requires, do we need attrdict/scipy
     install_requires = [
-        'dash>2', 'statsmodels','numpy','scipy', 'pandas', 'flask', 'plotly'
+        'dash>2', 'statsmodels','numpy','scipy', 'pandas', 'flask', 'plotly',
+        'dash-html-components', 'dash-core-components'
     ],
-    scripts = ['crispr_screen_viewer/launch_viewers.py'],
+    scripts = ['crispr_screen_viewer/main_page.py'],
     python_requires = '>=3.6',
     include_package_data=False,
 )
