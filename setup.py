1079118df05d22ed5e0e44ddae54e44051f82716
johnc.thomas <jct61@cam.ac.uk>
2022-04-08 11:52:45 +0100

    remove html-components from install_requires, bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'crispr_screen_viewer',
-    version = '0.6.1',
+    version = '0.6.2',
     author = 'John C. Thomas',
     author_email = 'jcthomas000@gmail.com',
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'junk_probably']),
     install_requires = [
         'dash>2', 'statsmodels','numpy','scipy', 'pandas', 'flask', 'plotly',
-        'dash-html-components', 'dash-core-components'
+        'dash-core-components'
     ],
     scripts = ['crispr_screen_viewer/launch.py'],
     python_requires = '>=3.6',
