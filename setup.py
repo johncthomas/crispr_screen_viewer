c2524d7c2173be0b1a293b09971cc54de11adf19
johnc.thomas <jct61@cam.ac.uk>
2023-01-13 11:32:12 +0000

    versioninfo

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'crispr_screen_viewer',
-    version = '0.8.0',
+    version = '0.8.0.proteomicsBiplots',
     author = 'John C. Thomas',
     author_email = 'jcthomas000@gmail.com',
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'junk_probably']),
     install_requires = [
         'dash>2', 'numpy','scipy', 'pandas', 'flask', 'plotly',
-        'dash-core-components', 'dash-bootstrap-components'
+        'dash-core-components', 'dash-bootstrap-components',
     ],
     scripts = ['crispr_screen_viewer/launch.py'],
     python_requires = '>=3.6',
