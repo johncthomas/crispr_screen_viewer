a847769a37dd51ada99691ce041d6848b0c7065a
johnc.thomas <jct61@cam.ac.uk>
2023-02-02 15:58:45 +0000

    rejustify footer

--- crispr_screen_viewer/launch.py
+++ crispr_screen_viewer/launch.py
@@ -158,7 +158,7 @@ def initiate_app(data_set:DataSet, public_version=False, urlbase='/'):
         ),
 
         Div(
-            className='center-flex',
+            className='spacey-flex',
             children=[
                 html.A(rel='noopener nofollow', target='_blank', href='https://www.cruk.cam.ac.uk/', children=[
                     html.Img(src="assets/images/cruk_cambridge_i_pos_cmyk.jpg", )
