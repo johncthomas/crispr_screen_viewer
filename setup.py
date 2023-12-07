a772b16c049a9624e6cd561381e5a5cc0b7fe4c3
jaytee <jcthomas000@gmail.com>
2023-12-07 19:11:10 +0000

    Backwards compatable setup.py when installing as editable.

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
-setup(
-    name = 'crispr_screen_viewer',
-    version = '0.8.0',
-    author = 'John C. Thomas',
-    author_email = 'jcthomas000@gmail.com',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'junk_probably']),
-    install_requires = [
-        'dash>2', 'numpy','scipy', 'pandas', 'flask', 'plotly',
-        'dash-core-components', 'dash-bootstrap-components', 'dash-bio',
-        'sqlalchemy'
-    ],
-    scripts = ['crispr_screen_viewer/launch.py'],
-    python_requires = '>=3.6',
-    include_package_data=False,
-)
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
